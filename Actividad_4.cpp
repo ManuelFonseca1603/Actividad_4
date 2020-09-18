1aa30e84f314519db6dbd9162fa01dba89c00327
Manuel Fonseca <manuel.fonseca45499@alumnos.udg.mx>
2020-09-18 11:35:25 -0500

    Cree el menu

---
+++ Actividad_4.cpp
@@ -0,0 +1,21 @@
+#include"stdio.h"
+
+main()
+{
+	int op;
+	
+	printf("Elige la opccion que quieres:\n"
+			"1.-Capturar numeros e imprimir promedio\n"
+			"2.-Capturar un arreglo e imprimirlo n veces\n"
+			"3.-Crear personaje\n"
+			"4.-salir\n");
+	scanf("%i",&op);
+	
+	switch(op)
+	{
+		case 1:
+		break;	
+	}
+	
+	return 0;		
+}
