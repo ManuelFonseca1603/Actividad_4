079798835b58f220771d3dc9bb4fbb098e311318
Manuel Fonseca <manuel.fonseca45499@alumnos.udg.mx>
2020-09-18 11:48:13 -0500

    Cree la funcion mostrar para almacenar 5 numeros y despues mostar la suma y el promedio

--- Actividad_4.cpp
+++ Actividad_4.cpp
@@ -1,11 +1,25 @@
 #include"stdio.h"
 
+void capturar(){
+	int i,numeros[5];
+	float s;
+	for(i=0;i<5;i++)
+	{
+	scanf("%i",&numeros[i]);
+	s+=numeros[i];
+	}
+	printf("Los numeros son: \n");
+	for(i=0;i<5;i++)
+	printf(" %i \n",numeros[i]);
+	printf("La suma es: %.2f\nEl promedio es: %.2f\n",s,s/5);
+}
+
 main()
 {
 	int op;
 	
 	printf("Elige la opccion que quieres:\n"
-			"1.-Capturar numeros e imprimir promedio\n"
+			"1.-Capturar numeros e imprimir suma y promedio\n"
 			"2.-Capturar un arreglo e imprimirlo n veces\n"
 			"3.-Crear personaje\n"
 			"4.-salir\n");
@@ -14,6 +28,8 @@ main()
 	switch(op)
 	{
 		case 1:
+			printf("Escribe los numeros: \n");
+			capturar();
 		break;	
 	}
 	
